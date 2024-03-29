2931db18a09eeb99e216d898a5408d26119291c8
qkrskdusdlqslek <dnftksdurh10@daum.net>
2024-03-29 17:09:08 +0900

    2일차 최종 정리

--- README.md
+++ README.md
@@ -84,7 +84,14 @@ IoT 개발자과정 SQLServer 학습 리포지토리
         - 집계함수와 GROUP BY
             - SUM(총합), AVG(평균), COUNT(개수), MAX(최대값), MIN(최소값) 
             - 집계함수 외 일반 컬럼은 GROUP BY 절에 속한 컬럼만 SELECT문에 사용가능
-            - HAVING은 집계함수의 필터로 GROUP BY 뒤에 작성, WHERE절과 필터링이 다르다
+            - HAVING은 집계함수의 필터로 GROUP BY 뒤에 작성, WHERE절과 필터링이 다르다.
+
+        - 두개 이상의 테이블 질의(Query)
+            - 관계형 DB에서 가장 중요한 기법 중 하나 : JOIN!
+            - INNER JOIN(내부 조인) 
+            - LEFT|RIGHT OUTER JOIN(외부 조인) - 어느 테이블이 기준인지에 따라서 결과가 상이함
+
+            ![외부조인](https://raw.githubusercontent.com/qkrskdusdlqslek/basic-database-2024/main/images/db004.png)
 
 
 
---
+++ day02/sql009_연습문제_Part2.sql
@@ -0,0 +1,35 @@
+﻿-- 연습문제 Part2
+-- 1. 마당서점 도서의 총 개수
+SELECT COUNT (bookid) AS [도서 총 개수]  -- (bookid) 대신 (*) 사용 가능
+  FROM Book;
+
+-- 2. 마당서점에 도서를 출고하는 출판사의 총 개수
+SELECT COUNT (DISTINCT publisher) AS [출판사의 총 개수]   
+  FROM Book;
+
+-- 3. 모든 고객의 이름, 주소
+SELECT [name]
+     , [address]
+  FROM Customer;
+
+-- 4. 2023년 7월 4일 ~ 7월 7일 사이에 주문 받은 도서의 주문번호
+SELECT orderid
+  FROM Orders
+ WHERE orderdate BETWEEN '2023-07-04' AND '2023-07-07';
+
+-- 5. 2023년 7월 4일 ~ 7월 7일 사이에 주문 받은 도서를 제외한 도서의 주문번호
+SELECT orderid
+  FROM Orders
+ WHERE orderdate NOT BETWEEN '2023-07-04' AND '2023-07-07';
+
+-- 6. 성이 '김'씨인 고객의 이름과 주소
+SELECT [name]
+     , [address]
+  FROM Customer
+ WHERE [name] LIKE '김%';
+
+-- 7. 성이 '김'씨이고 이름이 '아'로 끝나는 고객의 이름과 주소
+SELECT [name]
+     , [address]
+  FROM Customer
+ WHERE name LIKE '김%아';
