312a6cbd4de2c9e7d47ec75dec6011edd22d4f61
qkrskdusdlqslek <dnftksdurh10@daum.net>
2024-04-05 12:26:17 +0900

    7일차 중간 정리

---
+++ day07/sql028_s01_t1.sql
@@ -0,0 +1,22 @@
+-- 트랜잭션 1
+BEGIN TRAN;
+USE madang;
+SELECT * 
+FROM   Book
+WHERE  bookid=1;
+
+ 
+
+UPDATE Book 
+SET     price=7100
+WHERE  bookid=1;
+-- 1. 여기까지 실행
+
+SELECT * 
+FROM   Book
+WHERE  bookid=1;
+
+ 
+
+COMMIT;
+ROLLBACK;
